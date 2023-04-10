481f3c39aae53f9fe1726b9b8203194891968434
obendesmond <obendesmond2@gmail.com>
2023-04-10 20:09:02 +0100

    finish header and layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
+import Layout from "./components/Layout";
+
+interface AppTypes {
+  boardTypes: {
+    title: string;
+    todos: AppTypes["todo"][];
+  }[];
+
+  todo: {
+    title: string;
+    description: string;
+  };
+}
 
 const App: React.FC = () => {
+  const [boards, setBoards] = useState<AppTypes["boardTypes"]>([
+    {
+      title: "First Board",
+      todos: [{ title: "first todo", description: "some description" }],
+    },
+  ]);
+
   return (
-    <div>
+    <Layout>
       <Header />
-    </div>
+    </Layout>
   );
 };
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from "react";
+
+interface LayoutTypes {
+  IProps: {
+    children: ReactNode;
+  };
+}
+
+const Layout: React.FC<LayoutTypes["IProps"]> = ({ children }) => {
+  return <div className="bg-[#e5f3f7] h-[100vh]">{children}</div>;
+};
+
+export default Layout;
